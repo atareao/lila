d0a48c9793bb25f0422a2f30597fec467467df0c
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-06-22 11:58:37 +0200

    ✨ Añade la funcionalidad de inicio de sesión con OAuth y corrige errores menores en la interfaz

---
+++ src/utils/constants.rs
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2025 Lorenzo Carbonell
+//
+// This file is part of the LiLa project,
+// and is licensed under the MIT License. See the LICENSE file for details.
+
+pub const APP_ID: &str = "es.atareao.lila";
+pub const APP_QUALIFIER: &str = "es";
+pub const APP_ORGANIZATION: &str = "atareao";
+pub const APP_APPLICATION: &str = "lila";
+pub const APP_NAME: &str = "lila";
+pub const SCROLL_NAME: &str = "lila-scroll";
+pub const LISTBOX_NAME: &str = "lila-listbox";
